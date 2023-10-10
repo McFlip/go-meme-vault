5abdd8f0b9b31e64885be9be2c9e229014141c4c
McFlip <gradydenton@yahoo.com>
2023-10-10 00:26:52 -0400

    start index template

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/McFlip/go-meme-vault
 
 go 1.21.1
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"html/template"
 	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	log.Println("Hello World.")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/index.html"))
+		tmpl.Execute(w, nil)
+	})
+
+	http.ListenAndServe(":8080", r)
 }
