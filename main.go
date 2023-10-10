fd319aad70ca67370e2ec342624d1259fbabb43d
McFlip <gradydenton@yahoo.com>
2023-10-10 13:04:34 -0400

    htmx

--- main.go
+++ main.go
@@ -20,5 +20,10 @@ func main() {
 		tmpl.Execute(w, nil)
 	})
 
+	r.Get("/memes", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/memes.html"))
+		tmpl.Execute(w, nil)
+	})
+
 	http.ListenAndServe(":8080", r)
 }
