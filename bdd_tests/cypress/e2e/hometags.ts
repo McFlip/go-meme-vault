e1abce8536d6e4973e3a0188dfd11e6ab5cc9675
McFlip <gradydenton@yahoo.com>
2023-10-13 00:38:52 -0400

    install cypress testing library

--- bdd_tests/cypress/e2e/hometags.ts
+++ bdd_tests/cypress/e2e/hometags.ts
@@ -1,13 +1,14 @@
 import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
+import '@testing-library/cypress/add-commands'
 
 Given("I am on the home page", () => {
   cy.visit("localhost:8080/")
 })
 
 When("I scroll through the list of tags", () => {
-  console.log("TODO: implement scroll into view")
+  cy.findByRole('link', {name: 'third tag'}).scrollIntoView()
 })
 
 Then("I should see all the available tags", () => {
-  cy.get("#tag-list").should("contain.text", "test tag")
+  cy.get('main').findAllByRole('listitem').should('have.length', 3)
 })
